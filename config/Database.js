12830c29979cf1272342535c911500dc0458ee7e
Yaspa2314 <yaspa354@gmail.com>
2023-12-10 20:56:23 +0700

    yaspa

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 
 
-const db = new Sequelize('railway', 'root', '2a1h6hchAebaDfb634d-4aeHghdBfbg6', { 
+const db = new Sequelize('railway', 'root', '2a1h6hchAebaDfb634d-4aeHghdBfbg6', {
     host: 'monorail.proxy.rlwy.net',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: '15217'
 
 });
 
-module.exports=db;
+module.exports = db;
