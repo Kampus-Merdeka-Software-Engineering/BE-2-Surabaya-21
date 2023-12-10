48b0b032aaffdabdcb84de87561ea49e7a256b4a
Yaspa2314 <yaspa354@gmail.com>
2023-12-10 22:32:42 +0700

    yaspa

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,25 +1,67 @@
 
 const User = require('../models/UserModel');
-exports.CreateTestimoni = async (req, res) => {
-    try {
-        
-         const { nama, email, notelp, testimonial } = req.body;
-         if (!nama || !email || !testimonial) {
-            return res.status(400).json({
+
+
+class testimoniController {
+    async CreateTestimoni(req, res) {
+        try {
+            const { nama, email, notelp, testimonial } = req.body;
+            if (!nama || !email || !testimonial) {
+                return res.status(400).json({
+                    status: "fail",
+                    message: "Nama, email, and testimonial are required fields.",
+                });
+            }
+            const user = await User.create({ nama, email, notelp, testimonial });
+            res.status(201).json({
+                status: "success",
+                data: user
+
+            });
+        } catch (err) {
+            res.status(400).json({
                 status: "fail",
-                message: "Nama, email, and testimonial are required fields.",
+                message: err.message
             });
         }
-        const user = await User.create( { nama, email, notelp, testimonial });
-        res.status(201).json({
-            status: "success",
-            data:user
-
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            message: err.message
-        });
+    }
+    static async getAllTestimoni(req, res) {
+        try {
+            const user = await User.findAll();
+            res.status(200).json({
+                status: "success",
+                data: user
+            });
+        } catch (err) {
+            res.status(400).json({
+                status: "fail",
+                message: err.message
+            });
+        }
     }
 }
+module.exports = testimoniController;
+
+// exports.CreateTestimoni = async (req, res) => {
+//     try {
+        
+//          const { nama, email, notelp, testimonial } = req.body;
+//          if (!nama || !email || !testimonial) {
+//             return res.status(400).json({
+//                 status: "fail",
+//                 message: "Nama, email, and testimonial are required fields.",
+//             });
+//         }
+//         const user = await User.create( { nama, email, notelp, testimonial });
+//         res.status(201).json({
+//             status: "success",
+//             data:user
+
+//         });
+//     } catch (err) {
+//         res.status(400).json({
+//             status: "fail",
+//             message: err.message
+//         });
+//     }
+// }
