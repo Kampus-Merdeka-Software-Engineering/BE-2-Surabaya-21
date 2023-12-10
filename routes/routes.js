b922f13f7966bfbe75d324be0191922aaba873db
Yaspa2314 <yaspa354@gmail.com>
2023-12-10 22:35:41 +0700

    yaspa

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const path = require('path');
-const {CreateTestimoni} = require("../controllers/controllers");
+const {CreateTestimoni, getAllTestimoni} = require("../controllers/controllers");
 
 
 router.post("/CreateTestimoni",CreateTestimoni);
 router.get('/testimoni', (req, res) => {
     res.sendFile(path.join(__dirname, '../view/index.html'));
 });
+router.get('/getAllTestimoni', getAllTestimoni);
+
 module.exports = router;
